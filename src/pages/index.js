b42015f475e7f6a4d9bfab68cc055d72ef1ed240
Zhe Zhang <zhezhang@slac.stanford.edu>
2025-01-13 20:42:41 -0800

    Redirect to the new website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,15 @@
-import React from 'react'
-import clsx from 'clsx'
-import Layout from '@theme/Layout'
-import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import HomepageFeatures from '../components/HomepageFeatures'
-import BadgerCarousel from '../components/BadgerCarousel'
-import styles from './index.module.css'
+import React, { useEffect } from 'react';
 
-const HomepageHeader = () => {
-  const { siteConfig } = useDocusaurusContext()
+export default function Home() {
+  useEffect(() => {
+    window.location.href = "https://xopt-org.github.io/Badger/";
+  }, []);
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <BadgerCarousel />
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </header>
-  )
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>We've Moved!</h1>
+      <p>Please visit our new website:</p>
+      <p><a href="https://xopt-org.github.io/Badger/">https://xopt-org.github.io/Badger/</a></p>
+    </div>
+  );
 }
-
-const Home = () => {
-  const { siteConfig } = useDocusaurusContext()
-  return (
-    <Layout
-      title={`${siteConfig.title} the Optimizer`}
-      description="Official website of Badger the optimizer"
-    >
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  )
-}
-
-export default Home
